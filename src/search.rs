67bd22ac0d9c98b6cd66fd8c96fe2bf1d0c4678e
Nick Pezza <npezza93@gmail.com>
2020-01-19 17:16:12 -0500

    Add search tests

--- src/search.rs
+++ src/search.rs
@@ -65,3 +65,108 @@ impl<W: Write> Search<W> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Read;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test_render() {
+        let output = NamedTempFile::new().expect("err");
+        let mut search = Search::new("> ".to_string(), &output);
+        search.render();
+
+        let mut output = output.reopen().expect("err");
+        let mut actual = String::new();
+        output.read_to_string(&mut actual).expect("Err");
+
+        assert_eq!(
+            format!("{}\r> {}", cursor::clear_line(), cursor::col(3)),
+            actual
+        );
+    }
+
+    #[test]
+    fn test_keypress() {
+        let output = NamedTempFile::new().expect("err");
+        let mut search = Search::new("> ".to_string(), &output);
+        search.keypress('b');
+
+        let mut output = output.reopen().expect("err");
+        let mut actual = String::new();
+        output.read_to_string(&mut actual).expect("Err");
+
+        assert_eq!(
+            format!("{}\r> b{}", cursor::clear_line(), cursor::col(4)),
+            actual
+        );
+    }
+
+    #[test]
+    fn test_backspace() {
+        let output = NamedTempFile::new().expect("err");
+        let mut search = Search::new("> ".to_string(), &output);
+        search.query = vec!['a', 'b', 'c'];
+        search.position = 3;
+        search.backspace();
+
+        let mut output = output.reopen().expect("err");
+        let mut actual = String::new();
+        output.read_to_string(&mut actual).expect("Err");
+
+        assert_eq!(
+            format!("{}\r> ab{}", cursor::clear_line(), cursor::col(5)),
+            actual
+        );
+    }
+
+    #[test]
+    fn test_left() {
+        let output = NamedTempFile::new().expect("err");
+        let mut search = Search::new("> ".to_string(), &output);
+        search.query = vec!['a', 'b', 'c'];
+        search.position = 1;
+        search.render();
+        search.left();
+
+        let mut output = output.reopen().expect("err");
+        let mut actual = String::new();
+        output.read_to_string(&mut actual).expect("Err");
+
+        assert_eq!(
+            format!(
+                "{}\r> abc{}{}",
+                cursor::clear_line(),
+                cursor::col(4),
+                cursor::left(1)
+            ),
+            actual
+        );
+    }
+
+    #[test]
+    fn test_right() {
+        let output = NamedTempFile::new().expect("err");
+        let mut search = Search::new("> ".to_string(), &output);
+        search.query = vec!['a', 'b', 'c'];
+        search.position = 1;
+        search.render();
+        search.right();
+
+        let mut output = output.reopen().expect("err");
+        let mut actual = String::new();
+        output.read_to_string(&mut actual).expect("Err");
+
+        assert_eq!(
+            format!(
+                "{}\r> abc{}{}",
+                cursor::clear_line(),
+                cursor::col(4),
+                cursor::right(1)
+            ),
+            actual
+        );
+    }
+}
