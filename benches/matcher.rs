303295cbf87cd5788ee558023e67a68baa54719b
Nick Pezza <npezza93@gmail.com>
2020-01-31 21:49:59 -0500

    Update bencher to work with new impl

--- benches/matcher.rs
+++ benches/matcher.rs
@@ -1,13 +1,13 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use fozzie::choice::Choice;
 use fozzie::matcher;
 
 fn matches(c: &mut Criterion) {
     c.bench_function("matcher", |b| {
-        let choice = Choice::new(String::from("Gemfile"));
+        let choice = "Gemfile";
+        let query = ['g', 'e', 'm'];
 
-        b.iter(|| matcher::matches(vec!['g', 'e', 'm'], &choice));
+        b.iter(|| matcher::matches(&query, &choice));
     });
 }
 
--- src/matcher.rs
+++ src/matcher.rs
@@ -4,5 +4,5 @@ pub fn matches(needle: &[char], haystack: &str) -> bool {
     needle.is_empty()
         || needle
             .iter()
-            .all(|nchar| choice::contains(&haystack, *nchar))
+            .all(|&nchar| choice::contains(&haystack, nchar))
 }
