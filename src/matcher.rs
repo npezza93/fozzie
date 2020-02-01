be532b69c53575532b6e887d371c1664cbedf43b
Nick Pezza <npezza93@gmail.com>
2020-01-31 22:12:51 -0500

    Add match tests

--- src/matcher.rs
+++ src/matcher.rs
@@ -6,3 +6,31 @@ pub fn matches(needle: &[char], haystack: &str) -> bool {
             .iter()
             .all(|&nchar| choice::contains(&haystack, nchar))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_matches() {
+        assert!(matches(&['a'], "a"));
+        assert!(matches(&['a'], "abc"));
+        assert!(matches(&['a', 'b', 'c'], "abc"));
+        assert!(matches(&['A', 'B', 'C'], "abc"));
+        assert!(matches(&['a', 'b', 'c'], "a1b2c3"));
+        assert!(matches(&['a', 'b', 'c'], "a1b2c3"));
+        assert!(matches(&['t', 'e', 's', 't'], "t/e/s/t"));
+        assert!(matches(&['t', 'e', 's', 't'], "t💣e💣s💣t"));
+        assert!(matches(&['💣', '💣', '💣'], "t💣e💣s💣t"));
+
+        assert!(!matches(&['a', 'b', 'c'], "ab"));
+        assert!(!matches(&['a', 'b', 'c'], ""));
+
+        assert!(matches(&[], ""));
+        assert!(matches(&[], "ab"));
+
+        // UTF-8 case testing
+        assert!(matches(&['a'], "A"));
+        assert!(matches(&['A'], "a"));
+    }
+}
