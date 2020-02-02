344ffe70f1462c289969144429482fac49100706
Nick Pezza <npezza93@gmail.com>
2020-02-01 22:45:28 -0500

    Add character highlighting

--- src/color.rs
+++ src/color.rs
@@ -2,6 +2,10 @@ pub fn inverse(text: &str) -> String {
     format!("\x1B[7m{}\x1B[27m", text)
 }
 
+pub fn highlight(c: char) -> String {
+    format!("\x1B[33m{}\x1B[39m", c)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +13,10 @@ mod tests {
     #[test]
     fn test_inverse() {
         assert_eq!("\x1B[7mfoo\x1B[27m", inverse("foo"));
+    }
+
+    #[test]
+    fn test_highlight() {
+        assert_eq!("\x1B[33mf\x1B[39m", highlight('f'));
     }
 }
